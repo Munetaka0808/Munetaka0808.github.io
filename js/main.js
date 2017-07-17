556ec6c35993b643348e658b3741b39ca2350ecd
Munetaka0808 <munetaka0808@gmail.com>
2017-07-17 18:08:57 +0900

    Github Pages

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(function() {
     $("#test").click(test);
     $("#omikuji").click(omikuji);
-    $("#resultOmikuji").dialog();
 });
 
 function test() {
@@ -11,24 +10,31 @@ function test() {
 function omikuji() {
     switch (rand(0,6)) {
         case 0:
+            $("#resultOmikuji").dialog();
             $("#contentOmikuji").html("大吉");
             break;
         case 1:
+            $("#resultOmikuji").dialog();
             $("#contentOmikuji").html("中吉");
             break;
         case 2:
+            $("#resultOmikuji").dialog();
             $("#contentOmikuji").html("小吉");
             break;
         case 3:
+            $("#resultOmikuji").dialog();
             $("#contentOmikuji").html("吉");
             break;
         case 4:
+            $("#resultOmikuji").dialog();
             $("#contentOmikuji").html("末吉");
             break;
         case 5:
+            $("#resultOmikuji").dialog();
             $("#contentOmikuji").html("凶");
             break;
         case 6:
+            $("#resultOmikuji").dialog();
             $("#contentOmikuji").html("大凶");
             break;
     }
