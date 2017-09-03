11dc59ffa94a89c59d14a7a82a266a0a0d4064b9
Munetaka0808 <munetaka0808@gmail.com>
2017-09-03 18:25:00 +0900

    Github Pages

--- js/main.js
+++ js/main.js
@@ -40,21 +40,12 @@ function omikuji() {
     }
 }
 
-Vue.component('todo-item', {
-  props: ['todo'],
-  template: '<li>{{ todo.text }}</li>'
-})
-var app7 = new Vue({
-  el: '#app-7',
+var app = new Vue({
+  el: '#app',
   data: {
-    groceryList: [
-      { id: 0, text: 'Vegetables' },
-      { id: 1, text: 'Cheese' },
-      { id: 2, text: 'Whatever else humans are supposed to eat' }
-    ]
+    message: 'Hello My Page!'
   }
 })
-
 
 /* ---------- Other Function ---------- */
 
