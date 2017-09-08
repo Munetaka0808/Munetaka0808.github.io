e476dd0a2847a827b5fef706f2d90d239045274d
Munetaka0808 <munetaka0808@gmail.com>
2017-09-08 17:06:43 +0900

    Github Pages

--- js/tovue.js
+++ js/tovue.js
@@ -4,3 +4,19 @@ var hello = new Vue({
         message: 'Thank you for visiting my page on ' + new Date().toLocaleString()
     }
 })
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app = new Vue({
+  el: '#todo-list',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
